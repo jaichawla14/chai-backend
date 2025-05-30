98bb9dd7f700819d3d904e6e4166189c030a00e5
jaichawla14 <jaichawla1409gmail.com>
2025-05-30 12:19:17 +0530

    up untial async handler

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // require('dotenv').config()
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: './env'
@@ -8,7 +9,16 @@ dotenv.config({
 
 
 connectDB()
-
+.then(() => {
+  app.listen (process.env.PORT || 8000, () => {
+    console.log(`Server is Running at poet : ${process.env.PORT}`);
+    
+  })
+})
+.catch((err) => {
+  console.log("MONGODB connect failed", err);
+  
+})
 
 
 
