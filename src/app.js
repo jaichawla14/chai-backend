083d7a10a5adbf7369b352b308f080a38d007228
jaichawla14 <jaichawla1409gmail.com>
2025-06-02 18:37:11 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -10,8 +10,12 @@ app.use(cors({
 }))
 
 app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit:"16lb"}))
+app.use(express.urlencoded({extended: true, limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+import userRouter from './routes/user.routes.js'
+
+app.use("/api/v1/users", userRouter)
 
 export {app}
